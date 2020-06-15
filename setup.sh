8b46de5f69eb3f8f971878ee53df858a96754bda
Nathan Philliber <nphilliber@gmail.com>
2020-06-15 16:06:42 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+cd ~
+
 sudo apt update
 sudo apt upgrade -y
 
 sudo apt install vim -y
 
+# Firewall
 sudo apt install ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
@@ -11,4 +14,17 @@ sudo ufw enable
 
 sudo apt install git -y
 sudo apt install tmux -y
+
+# Config files
+wget https://raw.githubusercontent.com/NathanPhilliber/Config-Files/master/.tmux.conf -O ~/.tmux.conf
+wget https://raw.githubusercontent.com/NathanPhilliber/Config-Files/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/NathanPhilliber/Config-Files/master/.zshrc -O ~/.zshrc
+
+# ZSH and OH-MY-ZSH
+sudo apt install zsh -y
+chsh -s $(which zsh)
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+pwd | xargs -I {} sed -i.old '1s;^;export ZSH="{}.oh-my-zsh"\n;' .zshrc
+rm .zshrc.old
+
 
